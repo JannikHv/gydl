4ae6cb65f7bb4b69ec0164677e64120a7b5eef8f
Jannik Hauptvogel <JannikHv@users.noreply.github.com>
2017-04-03 20:17:48 +0200

    Various fixes and updates
    Fixed the download when ogg is selected as a format.
    
    Added some design updates regarding the text and widgets placement.
    
    Rewrote the code for more readability, stability and developability.

--- src/gydl.py
+++ src/gydl.py
@@ -1,266 +1,253 @@
 #!/usr/bin/env python3
 
+__author__     = "Jannik Hauptvogel"
+__credits__    = "rg3"
+__license__    = "MIT"
+__maintainer__ = "Jannik Hauptvogel"
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
+from subprocess    import call as subcall
+from os            import getenv
 
-from subprocess import call as subcall
+class GydlMessageGui(Gtk.Window):
 
-class ErrorGui(Gtk.Window):
+    def getLayout(self, errMessage):
+        label = Gtk.Label(errMessage)
+        return label
 
-    def getInterface(self):
-        btn  = Gtk.Button(label="Continue")
-        text = Gtk.Label ("")
-        fix  = Gtk.Fixed ()
+    def getHeaderBar(self, errTitle):
 
-        btn .set_size_request(350, 50)
-        text.set_size_request(350, 50)
-
-        btn.connect("clicked", Gtk.main_quit, None)
-        text.set_markup("<b>Internet Connection Error</b>")
-        fix.put(btn,  0, 50)
-        fix.put(text, 0, 0)
-
-        return fix
-
-    def __init__(self):
-        Gtk.Window.__init__(self)
-        self.set_default_size(350, 100)
-        self.set_resizable   (False)
-        self.set_title("Gydl - Connection Error")
-        self.set_icon_name("Youtube-youtube.com")
-        self.add(self.getInterface())
-
-class DlTrueGui(Gtk.Window):
-
-    def getInterface(self):
-        btn  = Gtk.Button(label="Continue")
-        text = Gtk.Label ("")
-        fix  = Gtk.Fixed()
-
-        btn .set_size_request(350, 50)
-        text.set_size_request(350, 50)
-
+        # Configuration of the headerbar
+        hbar  = Gtk.HeaderBar()
+        label = Gtk.Label(errTitle)
+        btn   = Gtk.Button(label="Continue")
         btn.connect("clicked", Gtk.main_quit)
-        text.set_markup("<b>Download finished successfully</b>")
-        fix.put(btn,  0, 50)
-        fix.put(text, 0, 0)
 
-        return fix
+        hbar.set_custom_title(label)
+        hbar.pack_end(btn)
+        hbar.set_show_close_button(False)
 
-    def __init__(self):
-        Gtk.Window.__init__(self)
-        self.set_default_size(350, 100)
+        return hbar
+
+    def __init__(self, errTitle, errMessage):
+
+        # Configure the window
+        Gtk.Window.__init__  (self)
+        self.set_default_size(375, 100)
         self.set_resizable   (False)
-        self.set_title("Gydl - Download Successful")
-        self.set_icon_name("Youtube-youtube.com")
-        self.add(self.getInterface())
+        self.set_border_width(10)
+        self.set_icon_name   ("Youtube-youtube.com")
+        self.set_titlebar    (self.getHeaderBar(errTitle))
+        self.set_position    (Gtk.WindowPosition.CENTER)
 
-class DlFalseGui(Gtk.Window):
+        self.add(self.getLayout(errMessage))
+        self.connect("delete-event", Gtk.main_quit)
+        self.show_all()
 
-    def getInterface(self):
-        btn  = Gtk.Button(label="Continue")
-        text = Gtk.Label ("")
-        fix  = Gtk.Fixed()
+class GydlMainGui(Gtk.Window):
 
-        btn .set_size_request(350, 50)
-        text.set_size_request(350, 50)
-
-        btn.connect("clicked", Gtk.main_quit)
-        text.set_markup("<b>Download finished unsuccessfully</b>")
-        fix.put(btn,  0, 50)
-        fix.put(text, 0, 0)
-
-        return fix
-
-    def __init__(self):
-        Gtk.Window.__init__(self)
-        self.set_default_size(350, 100)
-        self.set_resizable   (False)
-        self.set_title("Gydl - Download Unsuccessful")
-        self.set_icon_name("Youtube-youtube.com")
-        self.add(self.getInterface())
-
-class GydlGui(Gtk.Window):
-
-    def downloadMain(self, cmd):
-        print(cmd)
-        status = subcall(cmd, shell=True)
+    def setDownloadMain(self, Cmd, Type):
+        status = subcall(Cmd, shell=True)
 
         if status == 0:
-            Exec = DlTrueGui()
-            Exec.connect("delete-event", Gtk.main_quit)
-            Exec.show_all()
-        else:
-            Exec = DlFalseGui()
-            Exec.connect("delete-event", Gtk.main_quit)
-            Exec.show_all()
+            title   = ("Your download was successful")
+            message = ("Your " + Type + " has been downloaded successfully.\n" +
+                     "The file has been stored in " + getenv("HOME") + "/ .\n" +
+                     "Please press on continue to exit this program.")
 
-    def prepareVideo(self):
-        cmd = "youtube-dl -f FFF -f bestvideo[height=QQQ] \"UUU\" -o \"~/%(title)s.%(ext)s\""
+            GydlMessageGui(title, message)
+
+        else:
+            title   = ("Your download was unsuccessful")
+            message = ("Your " + Type + " has not been downloaded.\n" + 
+                       "Please press on continue to exit this program.")
+
+            GydlMessageGui(title, message)
+
+    def setDownloadVideo(self):
+
+        # Prepare the command
+        cmd = ("youtube-dl --noplaylist -f FFF -f bestvideo[height=QQQ] " +
+               "-o \"~/%(title)s.%(ext)s\" \"UUU\"")
 
         cmd = cmd.replace("FFF", self.vFormat.get_active_text())
         cmd = cmd.replace("QQQ", self.vQuality.get_active_text().replace("p", ""))
         cmd = cmd.replace("UUU", self.vEntry.get_text())
 
-        self.downloadMain(cmd)
+        self.setDownloadMain(cmd, "video")
 
-    def prepareAudio(self):
+    def setDownloadAudio(self):
 
-        cmd = "youtube-dl -x --audio-format FFF --audio-quality QQQ \"UUU\" -o \"~/%(title)s.%(ext)s\""
+        # Prepare the command
+        cmd = ("youtube-dl --no-playlist -x --audio-format FFF " +
+               "--audio-quality QQQ -o \"~/%(title)s.%(ext)s\" \"UUU\"")
 
-        cmd = cmd.replace("FFF", self.aFormat.get_active_text())
+        if self.aFormat.get_active_text() == "ogg":
+            cmd = cmd.replace("FFF", "vorbis")
+        else:
+            cmd = cmd.replace("FFF", self.aFormat.get_active_text())
+
         cmd = cmd.replace("QQQ", self.aQuality.get_active_text()[0])
         cmd = cmd.replace("UUU", self.aEntry.get_text())
 
-        self.downloadMain(cmd)
+        self.setDownloadMain(cmd, "audio")
 
-    def prepareDownload(self, widget):
+    def setDownloadPrepare(self, widget):
 
+        # Hide the main window
         self.hide()
         Gdk.flush()
 
+        # Check if internet connection is present
         status = subcall("ping -c 1 google.com", shell=True)
-
         if status != 0:
-            self.destroy()
-            Exec = ErrorGui()
-            Exec.connect("delete-event", Gtk.main_quit)
-            Exec.show_all()
-
-        if self.stack.get_visible_child_name() == 'A':
-            self.prepareAudio()
+            GydlMessageGui("Connection Error", "No internet connection has been established.\n" +
+                                               "Please press on continue to exit this program.")
         else:
-            self.prepareVideo()
+            # Find out if Video or Audio is selected
+            if self.stack.get_visible_child_name() == "A":
+                self.setDownloadAudio()
+            else:
+                self.setDownloadVideo()
 
-    def buildVideoStack(self):
-        self.vEntry   = Gtk.Entry()
-        self.vQuality = Gtk.ComboBoxText()
-        self.vFormat  = Gtk.ComboBoxText()
-        eLabel        = Gtk.Label("")
-        fLabel        = Gtk.Label("")
-        qLabel        = Gtk.Label("")
-        fix           = Gtk.Fixed()
+    def getVideoArea(self):
 
-        self.vEntry.set_placeholder_text("https://youtube.com/watch...")
+        # Create basic widgets
+        self.vEntry    = Gtk.Entry()
+        self.vFormat   = Gtk.ComboBoxText()
+        self.vQuality  = Gtk.ComboBoxText()
+        eLabel         = Gtk.Label()
+        fLabel         = Gtk.Label()
+        qLabel         = Gtk.Label()
+        fix            = Gtk.Fixed()
 
-        eLabel.set_markup("<b>Enter the URL</b>")
-        fLabel.set_markup("<b>Format </b>"      )
-        qLabel.set_markup("<b>Quality</b>"      )
-
-        # Build the format combobox
+        # Add entries to comboboxes
         for i in ["3gp", "flv", "mp4", "webm"]:
             self.vFormat.append_text(i)
 
-        # Build the quality combobox
         for i in ["2160p", "1440p", "1080p", "720p",
                   "480p",  "360p",  "240p",  "144p"]:
             self.vQuality.append_text(i)
 
-        # Size the widgets
-        self.vEntry  .set_size_request(500, 30)
-        self.vQuality.set_size_request(250, 30)
-        self.vFormat .set_size_request(250, 30)
-        eLabel       .set_size_request(500, 30)
-        fLabel       .set_size_request(250, 30)
-        qLabel       .set_size_request(250, 30)
-
-        # Add the widgets to the environment
-        fix.put(self.vEntry,   0,   35 )
-        fix.put(self.vFormat,  0,   125)
-        fix.put(self.vQuality, 250, 125)
-        fix.put(eLabel,        0,   0  )
-        fix.put(fLabel,        0,   90 )
-        fix.put(qLabel,        250, 90 )
-
         self.vFormat .set_active(2)
         self.vQuality.set_active(3)
 
-        self.stack.add_titled(fix, "V", "Video")
+        # Add text to widgets
+        eLabel.set_markup("<big>Enter the URL</big>" )
+        fLabel.set_markup("<big><u>Format</u></big>" )
+        qLabel.set_markup("<big><u>Quality</u></big>")
 
-    def buildAudioStack(self):
-        self.aEntry   = Gtk.Entry()
-        self.aQuality = Gtk.ComboBoxText()
-        self.aFormat  = Gtk.ComboBoxText()
-        eLabel        = Gtk.Label("")
-        fLabel        = Gtk.Label("")
-        qLabel        = Gtk.Label("")
-        fix           = Gtk.Fixed()
+        # Sizing the widgets
+        self.vEntry   .set_size_request(500, 30)
+        self.vFormat  .set_size_request(250, 30)
+        self.vQuality .set_size_request(250, 30)
+        eLabel        .set_size_request(500, 30)
+        fLabel        .set_size_request(250, 30)
+        qLabel        .set_size_request(250, 30)
 
-        self.aEntry.set_placeholder_text("https://youtube.com/watch...")
+        # Add the widgets to the interface
+        fix.put(self.vEntry,    0,   50 )
+        fix.put(self.vFormat,   0,   140)
+        fix.put(self.vQuality,  250, 140)
+        fix.put(eLabel,         0,   10 )
+        fix.put(fLabel,         0,   105)
+        fix.put(qLabel,         250, 105)
 
-        eLabel.set_markup("<b>Enter the URL</b>")
-        fLabel.set_markup("<b>Format </b>"      )
-        qLabel.set_markup("<b>Quality</b>"      )
+        return fix
 
-        # Build the format combobox
+    def getAudioArea(self):
+
+        # Create basic widgets
+        self.aEntry    = Gtk.Entry()
+        self.aFormat   = Gtk.ComboBoxText()
+        self.aQuality  = Gtk.ComboBoxText()
+        eLabel         = Gtk.Label()
+        fLabel         = Gtk.Label()
+        qLabel         = Gtk.Label()
+        fix            = Gtk.Fixed()
+
+        # Add entries to comboboxes
         for i in ["aac", "m4a", "mp3", "ogg", "wav"]:
             self.aFormat.append_text(i)
 
-        # Build the quality combobox
         self.aQuality.append_text("0 (Best)")
+
         for i in range(1,9):
             self.aQuality.append_text(str(i))
 
         self.aQuality.append_text("9 (Worst)")
 
+        self.aFormat .set_active(2)
+        self.aQuality.set_active(5)
+
+        # Add text to widgets
+        eLabel.set_markup("<big>Enter the URL</big>" )
+        fLabel.set_markup("<big><u>Format</u></big>" )
+        qLabel.set_markup("<big><u>Quality</u></big>")
+
         # Size the widgets
         self.aEntry  .set_size_request(500, 30)
-        self.aQuality.set_size_request(250, 30)
         self.aFormat .set_size_request(250, 30)
+        self.aQuality.set_size_request(250, 30)
         eLabel       .set_size_request(500, 30)
         fLabel       .set_size_request(250, 30)
         qLabel       .set_size_request(250, 30)
 
-        # Add the widgets to the environment
-        fix.put(self.aEntry,   0,   35 )
-        fix.put(self.aFormat,  0,   125)
-        fix.put(self.aQuality, 250, 125)
-        fix.put(eLabel,        0,   0  )
-        fix.put(fLabel,        0,   90 )
-        fix.put(qLabel,        250, 90 )
+        # Add the widgets to the interface
+        fix.put(self.aEntry,    0,   50 )
+        fix.put(self.aFormat,   0,   140)
+        fix.put(self.aQuality,  250, 140)
+        fix.put(eLabel,         0,   10 )
+        fix.put(fLabel,         0,   105)
+        fix.put(qLabel,         250, 105)
 
-        self.aFormat .set_active(2)
-        self.aQuality.set_active(5)
+        return fix
 
-        self.stack.add_titled(fix, "A", "Audio")
+    def getHeaderBar(self, switch):
 
-    def getHeaderBar(self):
-        self.switcher = Gtk.StackSwitcher()
-        hbar          = Gtk.HeaderBar()
-        btn_dl        = Gtk.Button(label="Download ")
+        # Create download button
+        bDownload = Gtk.Button(label=" Download")
+        bDownload.connect("clicked", self.setDownloadPrepare)
+        bDownload.set_always_show_image(True)
+        bDownload.set_image_position(Gtk.PositionType.RIGHT)
+        bDownload.set_image(Gtk.Image.new_from_icon_name(
+                            "folder-download-symbolic",
+                            Gtk.IconSize.BUTTON))
 
-        btn_dl.connect("clicked", self.prepareDownload)
-        btn_dl.set_always_show_image(True)
-        btn_dl.set_image_position(Gtk.PositionType.RIGHT)
-        btn_dl.set_image(Gtk.Image.new_from_icon_name(
-                         "folder-download-symbolic",
-                         Gtk.IconSize.BUTTON))
-
-        hbar.pack_end        (btn_dl)
-        hbar.set_custom_title(self.switcher)
+        # Configuration of the headerbar
+        hbar = Gtk.HeaderBar()
         hbar.set_show_close_button(True)
+        hbar.set_custom_title(switch)
+        hbar.pack_end(bDownload)
 
         return hbar
 
     def __init__(self):
 
+        # Declare main variables
+        switch = Gtk.StackSwitcher()
+        self.stack  = Gtk.Stack()
+
         # Configuring the window
-        Gtk.Window.__init__(self)
-        self.connect         ("delete-event", Gtk.main_quit)
+        Gtk.Window.__init__  (self)
         self.set_default_size(500, 250)
         self.set_resizable   (False)
         self.set_border_width(10)
-        self.set_title       ("Graphical Youtube-dl")
-        self.set_titlebar    (self.getHeaderBar())
-        self.set_icon_name("Youtube-youtube.com")
+        self.set_icon_name   ("Youtube-youtube.com")
+        self.set_titlebar    (self.getHeaderBar(switch))
+        self.set_position    (Gtk.WindowPosition.CENTER)
 
-        self.stack = Gtk.Stack()
-        self.buildAudioStack()
-        self.buildVideoStack()
-        self.switcher.set_stack(self.stack)
+        self.stack.add_titled(self.getAudioArea() , "A", "Audio" )
+        self.stack.add_titled(self.getVideoArea() , "V", "Video" )
+        switch.set_stack(self.stack)
         self.add(self.stack)
 
-Win = GydlGui()
-Win.show_all()
-Gtk.main()
+
+
+if __name__ == "__main__":
+    Win = GydlMainGui()
+    Win.connect("delete-event", Gtk.main_quit)
+    Win.show_all()
+    Gtk.main()
